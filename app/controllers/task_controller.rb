bf55e8ffffc6ab5a88d9d6a0000ef4fd0d7fedad
xparta <tomarobaski@gmail.com>
2014-03-24 08:08:20 +0300

    addadded styled tables and ohter css details

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,5 @@
 class TaskController < ApplicationController
   def index
-  	@task_array = [ "Buy Milk", "Buy Soap", "Pay bill", "Draw Money" ]
   	#@model = Model.find(params[:id])
   end
 
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,4 @@
 class Model < ActiveRecord::Base
+
+
 end
